f2b353b29c1c68aae1512a42a86489a81292d858
not-s <abdusamatovf4@gmail.com>
2025-05-18 20:36:02 +0500

    install black

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -7,4 +7,9 @@ main_app = FastAPI()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app=settings.run.app, host=settings.run.host, port=settings.run.port, reload=settings.run.reload,)
+    uvicorn.run(
+        app=settings.run.app,
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=settings.run.reload,
+    )
