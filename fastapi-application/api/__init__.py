8974fe93d73d36efca55e9623017427a1d1e30a9
not-s <abdusamatovf4@gmail.com>
2025-05-19 18:16:41 +0500

    created api v1 and prefix config

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .api_v1 import router as api_v1_router
 
 router = APIRouter(prefix=settings.api.prefix)
+router.include_router(api_v1_router)
