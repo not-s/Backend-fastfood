f8ee3c4166ec3c0b1c7b876fb14fbdef0a5aa09e
not-s <abdusamatovf4@gmail.com>
2025-05-19 23:11:54 +0500

    fix session factory in db_helper.py

--- fastapi-application/core/db_helper.py
+++ fastapi-application/core/db_helper.py
@@ -29,7 +29,7 @@ class DatabaseHelper:
         self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
-            auto_commit=False,
+            autocommit=False,
             expire_on_commit=False,
         )
 
